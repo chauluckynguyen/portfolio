28762dbd802bad395eb90e625052cdcdde8c50ec
lucky.nguyen <lucky.nguyen@servicenow.com>
2019-01-04 15:32:07 -0800

    :art: Move global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { persistor, store } from '../store'; 
 
+import "./layout.css"
+
 const TemplateWrapper = ({ children }) => (
   <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>

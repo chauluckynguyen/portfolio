8d27b380e90d49619b3948d361ccef45133a64d5
lucky.nguyen <lucky.nguyen@servicenow.com>
2019-08-12 23:36:18 -0700

    Edit package, gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
 	},
 	plugins: [
 		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-sass`
+		`gatsby-plugin-sass`,
+		{
+			resolve: `gatsby-plugin-s3`,
+			options: {
+				bucketName: 'luckynguyen.me',
+				acl: null
+			},
+		}
 	],
 };
